89af12ac0f59214fb463464db6108c1387ecdfff
Deadshot <shujaakba2020@gmail.com>
2021-06-24 13:00:02 +0500

    Added Cpntact model and updated Contact section.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import Contact
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'portfolio/index.html', {})
+    alert = ''
+    if request.method == "POST":
+        full_name = request.POST['full_name']
+        email = request.POST['email']
+        message = request.POST['message']
+        print(f'Name: {full_name}, Email: {email}, Message: {message} ')
+        if full_name == "":
+            messages.warning(request, 'Please Enter your Name')
+        elif email == "":
+            messages.warning(request, 'Please Enter your Email')
+        elif message == "":
+            messages.warning(request, 'Please Enter your Message')
+        else:
+            Contact.objects.create(name=full_name, email=email, message=message)
+            messages.success(request, 'Thanks for reaching me, I have received your Message.')
+
+    return render(request, 'portfolio/index.html', {'alert': alert})
+
